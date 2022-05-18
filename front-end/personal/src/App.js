5347c28c691114983ccf97006faadb4fcac76465
Lindsay Wilhelm <lindsaywilhelm@Lindsays.local>
2022-05-18 10:49:04 -0400

    adds navigation to front-end and creates back-end server

--- front-end/personal/src/App.js
+++ front-end/personal/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// Components
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/portfolio'}>Portfolio</Link>
+        <Link to={'/contact'}>Contact</Link>
+      <Routes>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/portfolio' component={Portfolio}/>
+        <Route path='/contact' component={Contact}/>
+      </Routes>
+      </Router>
     </div>
   );
 }
---
+++ front-end/personal/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// Components
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import Portfolio from './Portfolio';
+
+function Nav() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Nav
